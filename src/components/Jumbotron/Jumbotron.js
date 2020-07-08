7715df13551bb97f2bac35811cc5f3d521dfaf5d
J <J@Ivas-MacBook.fios-router.home>
2020-07-07 22:46:15 -0400

    styling changes

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,8 +5,8 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
 <div className="jumbotron jumbotron-fluid">
   <div className="container text-center">
-    <h1 className="display-4">Friends Clicky Game!</h1>
-    <p className="lead">Click on any image to earn points, but don't click on any more than once!</p>
+    <h1 className="display-4">Emoji Clicky Game!</h1>
+    <p className="lead">Click on any emoji to earn points, but don't click on an emoji more than once!</p>
   </div>
 </div>
 );
